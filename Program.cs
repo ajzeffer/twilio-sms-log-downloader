715bcffe9fb8c2d7d5a49eeba7cdbdc1ea23e493
Adam Zeffer <az@prodiginet.com>
2020-01-22 05:58:18 -0700

    init

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace twilio_downloader
+{
+    public class ExportRecord{
+        public string sid{get;set;}
+        public string status {get;set;}
+        public string datesent {get;set;}
+        public string datecreated {get;set;}
+        public string lastupdated{get;set;}
+        public string to{get;set;}
+        public string url;
+    }
+    class Program
+    {
+        private static string accountSid = ""; 
+        private static string authToken = ""; 
+        static void Main(string[] args)
+        {
+            
+            var dateToRun = args.Length >= 1 ?  
+                    args[0] : 
+                    DateTime.Now.AddDays(-1).ToShortDateString();
+
+            var fileName = $"twilio-logs.csv";
+            Console.WriteLine($"Fetching calls for {dateToRun}");
+            var path = $"./{fileName}"; 
+            
+            if (!System.IO.File.Exists(path))
+            {
+               using (System.IO.FileStream fs = System.IO.File.Create(path)){
+                Console.WriteLine("File Written");
+                }
+            }
+            
+            using (var writer = new StreamWriter(fileName))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {    
+                TwilioClient.Init(accountSid, authToken);
+                var opts = new ReadMessageOptions(){
+                        PageSize = 100000, 
+                        DateSentAfter =DateTime.Parse($"{dateToRun} 00:00:00")
+                };
+                var messages = MessageResource.Read(opts);
+                var exportRecords = messages.Select(x => new ExportRecord{
+                    datecreated = x.DateCreated.ToString(), 
+                    datesent = x.DateSent.ToString(), 
+                    lastupdated = x.DateUpdated.ToString(), 
+                    sid = x.Sid, 
+                    status = x.Status.ToString(), 
+                    url = x.Uri.ToString(), 
+                    to = x.To.ToString()
+                });
+                csv.WriteRecords(exportRecords);
+            }
+        }
+        public static void WriteArgs(string[] args){
+                Console.WriteLine(String.Join(",",args));
+        }
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Twilio Log Exporter 
+
+There isn't currently any paging we just try and get x records in one call. 
+
+# Mac users 
+you may need to call the dotnet run command with sudo as we are writing to the filesystem and dotnet doesn't have access to write to the fs with out running in an elevated shell
